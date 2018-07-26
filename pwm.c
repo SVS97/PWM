2512a65a7bbdd8c7b25f26b78dc164638939024d
SVS97 <SVS97@i.ua>
2018-07-26 18:26:32 +0300

    first commit

--- pwm.c
+++ pwm.c
@@ -5,7 +5,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define PWM OCR1B = OCR1A 
+#define PWM OCR1B = (OCR1A )
 
 double myrand(int *x)
 {
@@ -20,7 +20,7 @@ double myrand(int *x)
 void timer1_init(void) {
 	/*initializing the timer*/
 	
-	TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1B0) | (1 << WGM11);		/* It is reset to "0" in the live count, when TCNT1 = OCR1A (/1024)	*/						
+	TCCR1A = (1 << COM1A1) | (1 << COM1A0) | (1 << COM1B1) | (1 << COM1B0) | (1 << WGM11);		/* It is reset to "0" in the live count, when TCNT1 = OCR1A (/1024)	*/						
 	TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10) | (1 << CS12);		/* CTC (Reset on coincidence)						*/
 	TCNT1 = 0x00;									/* timer state (initialization)						*/
 	
@@ -46,35 +46,35 @@ int main(void)
 		res = myrand(&x0);
 		// imitate probability density
 		if (res >= 65536) {
-			PWM = 2;  // maximum pwm level
+			PWM = 10;  // maximum pwm level
 			} else if (res > 50000) {
-			PWM = 3;
-			} else if (res > 40078) {
-			PWM = 5;
-			} else if (res > 30801) {
-			PWM = 7;
-			} else if (res > 27524) {
-			PWM = 10;
-			} else if (res > 24247) {
 			PWM = 15;
-			} else if (res > 20971) {
+			} else if (res > 40078) {
 			PWM = 20;
-			} else if (res > 17694) {
-			PWM = 25;
-			} else if (res > 14417) {
+			} else if (res > 30801) {
 			PWM = 30;
-			} else if (res > 11140) {
-			PWM = 35;
-			} else if (res > 7863) {
+			} else if (res > 27524) {
 			PWM = 40;
-			} else if (res > 4587) {
-			PWM = 45;
-			} else if (res > 1965) {
+			} else if (res > 24247) {
 			PWM = 50;
-			} else if (res > 1310) {
-			PWM = 55;
-			} else if (res > 654) {
+			} else if (res > 20971) {
 			PWM = 60;
+			} else if (res > 17694) {
+			PWM = 70;
+			} else if (res > 14417) {
+			PWM = 80;
+			} else if (res > 11140) {
+			PWM = 90;
+			} else if (res > 7863) {
+			PWM = 100;
+			} else if (res > 4587) {
+			PWM = 110;
+			} else if (res > 1965) {
+			PWM = 120;
+			} else if (res > 1310) {
+			PWM = 130;
+			} else if (res > 654) {
+			PWM = 150;
 			} else {
 			PWM = 0;  // logical 0
 		}
